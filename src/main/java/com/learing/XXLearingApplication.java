2ef043e2b92e23c5d2f88cb5fb69c2b335ce7405
lifang <xxplus@outlook.com>
2020-08-07 10:45:08 +0800

    springboot servlet

--- src/main/java/com/learing/XXLearingApplication.java
+++ src/main/java/com/learing/XXLearingApplication.java
@@ -1,13 +1,26 @@
 package com.learing;
 
+import com.learing.servlet.MyServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ServletComponentScan
 public class XXLearingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XXLearingApplication.class, args);
+    }
+
+
+    @Bean
+    public ServletRegistrationBean<MyServlet> getServletRegistrationBean(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new MyServlet());
+        bean.setLoadOnStartup(1);
+        return bean;
     }
 
 }
---
+++ src/main/java/com/learing/servlet/MyServlet.java
@@ -0,0 +1,19 @@
+package com.learing.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "MyServlet", urlPatterns = "/my-servlet")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello my-servlet!!!");
+        super.doGet(req, resp);
+    }
+
+}
