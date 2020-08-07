4830c08f727cd113ec61e347b4deb1d0de2d4388
lifang <xxplus@outlook.com>
2020-08-07 21:41:33 +0800

    springboot thymeleaf

--- src/main/java/com/learning/controller/HelloController.java
+++ src/main/java/com/learning/controller/HelloController.java
@@ -1,16 +1,25 @@
 package com.learning.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello(HttpSession session){
         session.setAttribute("hello", "hello springboot");
         return "hello springboot";
+    }
+
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(ModelMap map){
+        map.put("name", "hello thymeleaf");
+        return "thymeleaf";
     }
 }
