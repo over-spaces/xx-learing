5d3da084f92c5b1b08e74ea67852f57ecd8f4101
lifang <xxplus@outlook.com>
2020-08-07 10:21:46 +0800

    first commit

---
+++ src/main/java/com/learing/XXLearingApplication.java
@@ -0,0 +1,13 @@
+package com.learing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XXLearingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XXLearingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learing/XxLearingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxLearingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
